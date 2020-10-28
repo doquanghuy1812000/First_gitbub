7d1b25cd8d7127244d3f7de3868bca21538cf761
quanghuy18 <66511570+quanghuy18@users.noreply.github.com>
2020-10-28 21:50:28 +0700

    Them mot dong

--- Bai1.cpp
+++ Bai1.cpp
@@ -11,4 +11,5 @@ int main() {
 	c = (log(pow(x, 4) + pow(5, x)) / log(7)) +(log(pow(x, 2) + pow(5, b)) / log(exp(1)));
 	A = (pow(x, 3) + sin(b) - exp(0.19238)) / (5 + exp(b) + pow(c , 0.202345 + x));
 	printf("Ket qua: %.5f", A);
+	printf("anh đi tìm");
 }
